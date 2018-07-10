70529057fbd844ff38f9f659fd681080b23ad9db
bdemers1055 <bdemers1055@gmail.com>
2018-07-09 20:49:02 -0400

    elvis to make the order change

--- src/App.js
+++ src/App.js
@@ -2,13 +2,33 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      slide: 1
+    };
+}
+pullSlideIntoFrame(e){
+  this.setState({
+    slide: this.state.slide - 1
+  });
+}
+pullSlideOutOfFrame(e){
+  this.setState({
+    slide: this.state.slide + 1
+  });
+}
   render() {
     return (
-      <div>
-        <div className="slider-frame">
-          <div className="slide active"><img src="https://placeimg.com/500/300/animals" alt="animals" /></div>
-          <div className="slide"><img src="https://placeimg.com/500/300/nature" alt="nature" /></div>
-          <div className="slide"><img src="https://placeimg.com/500/300/arch" alt="architecture" /></div>
+      <div className="flex">
+        <div className="sliderFrame container">
+          {this.state.slide === 1 ? <div className="slide1"><img src="https://placeimg.com/500/300/animals" alt="animals" /></div>: null}
+          {this.state.slide === 2 ? <div className="slide2"><img src="https://placeimg.com/500/300/nature" alt="nature" /></div>: null}
+          {this.state.slide === 3 ? <div className="slide3"><img src="https://placeimg.com/500/300/arch" alt="arch" /></div>: null}
+        </div>
+        <div className="container controls">
+          <button className="previousArrow" onClick = {this.pullSlideOutOfFrame.bind(this)}>previous</button>
+          <button className="nextArrow" onClick = {this.pullSlideIntoFrame.bind(this)}>next</button>
         </div>
       </div>
     );
